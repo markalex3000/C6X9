30f5a88189efddb5408125c44ab7cd76634f9636
Mark Alexieff <mark.alexieff@outlook.com>
2019-04-17 22:25:32 -0700

    Moved vector<char> user_input inside main() rather than having it hang out as a global.  Moved main() to follow the function declarations and added comments.

--- C6X9/Source.cpp
+++ C6X9/Source.cpp
@@ -14,27 +14,57 @@ Standard opening
 global variables
 */
 
-vector<char> user_input;
+
 
 /*
 functions declarations
 */
 
 bool yes_no(string s);
-bool get_user_input(vector<char>& vc);
-void output(vector<char>& vc);
-int make_integer(vector<char>& vc);
-string make_string(vector<char>& vc);
+bool get_user_input(vector<char>& vc);		//get the vector of character digits
+void output(vector<char>& vc);				//send the vector to cout digit/char by digit
+int make_integer(vector<char>& vc);			//takes the vector and return the integer value
+string make_string(vector<char>& vc);		//takes the vecto and returns a string description of the integer
 
 /*
-fucntion definitions
+main
 */
 
-bool yes_no(string s) {
+int main() {
+
+	vector<char> user_input;
+	bool return_value = { false };   // return_value is not used anywhere - seemed like a good idea tho
+
+	/*
+	Welcoming message
+	*/
+
+	cout << "Welcome to C6x9\nThis program takes a serires of 4 chracters between 0 and 9 inclusive.\n";
+	cout << "It returns a statement of the general form WXYZ is W thousands(s) and X hundred(s) and Y ten(s) and Z one(s).\n";
+
+	/*
+	Start the program/play again loop
+	*/
+	do {
+		cout << "Enter up to four digits > ";
+		user_input.clear();								// clears the content of the user_input vector for each iteration
+		return_value = get_user_input(user_input);
+		output(user_input);
+		cout << "The Integer value is: " << make_integer(user_input) << "\n";
+		cout << make_integer(user_input) << " is " << make_string(user_input) << "\n";
+	} while (yes_no("\n Play again? "));
+}
+
+
+/*
+funcion definitions
+*/
+
+bool yes_no(string s) {						// self exlanatory - y return true anything else retirns false
 	char response{ 'X' };
 	cout << s << " (y/n): ";
 	cin >> response;
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');   //clears all remaining content from cin
 
 	switch (response)
 	{
@@ -47,20 +77,18 @@ bool yes_no(string s) {
 	}
 }
 
-bool get_user_input(vector<char>& vc) {
+bool get_user_input(vector<char>& vc) {      //gets the 4 digits of input - puts in vector provided
 	char temp = { 0 };
 	int digit_count = { 0 };
 	
 	while (cin.get(temp) && temp != '\n') {
-		if (!isdigit(temp)) simple_error("You can only enter digits...\n\n");
+		if (!isdigit(temp)) simple_error("You can only enter digits...\n\n");     //could be more forgiving
 		else {
-			if (digit_count < 4) {
+			if (digit_count < 4) {			// '4' hear should be a constant
 				vc.push_back(temp);
 				digit_count++;
 			}
 			else simple_error("Too many digits - only take 4...\n\n");
-
-			
 		}
 	}
 	return true;
@@ -68,24 +96,24 @@ bool get_user_input(vector<char>& vc) {
 
 void output(vector<char>& vc) {
 	cout << "The following digits were input: ";
-	for (auto it = begin(vc); it != end(vc); ++it) cout << *it;
-	cout << "\n\n";
+	for (auto it = begin(vc); it != end(vc); ++it) cout << *it;			//iterators seem pretty cool
+	cout << "\n\n";														// 'it' is a pointer apparently
 }
 
-int make_integer(vector<char>& vc)  {
+int make_integer(vector<char>& vc)  {									//takes the vector and produces and return its value as an integer
 	int the_integer = { 0 };
 	int power_of_ten = { 1 };
 	int countdown = vc.size() - 1;
 
 	for (auto it = begin(vc); it != end(vc); ++it) {
 		power_of_ten = pow(10,countdown);
-		the_integer += (*it - '0' )* power_of_ten;
+		the_integer += (*it - '0' )* power_of_ten;    // *it is a char and subtracing '0' gets the integer value of the char
 		--countdown;
 	}
 	return the_integer;
 }
 
-string make_string(vector<char>& vc)
+string make_string(vector<char>& vc)									//takes the vector and produces the descriptive string
 {
 	vector<string> powers_of_ten = { " thousand", " hundred", " ten", " one" };
 	string the_string = { NULL };
@@ -94,7 +122,7 @@ string make_string(vector<char>& vc)
 	int power_of_ten = { 1 };
 	int countdown = vc.size();
 
-	for (auto it = begin(vc); it != end(vc); ++it) {
+	for (auto it = begin(vc); it != end(vc); ++it) {		//it just works			
 		if (*it == '0') temp_string.append(" no");
 		else temp_string.append(1,*it);
 		temp_string.append(powers_of_ten[powers_of_ten.size() - countdown]);
@@ -106,23 +134,5 @@ string make_string(vector<char>& vc)
 	return the_string;
 }
 
-int main() {
 
-	bool return_value = { false };
-
-	cout << "Welcome to C6x9\nThis program takes a serires of 4 chracters between 0 and 9 inclusive.\n";
-	cout << "It returns a statement of the general form WXYZ is W thousands(s) and X hundred(s) and Y ten(s) and Z one(s).\n";
-
-	/*
-	Start the program/play again loop
-	*/
-	do {
-		cout << "Enter up to four digits > ";
-		user_input.clear();
-		return_value = get_user_input(user_input);
-		output(user_input);
-		cout << "The Integer value is: " << make_integer(user_input) << "\n";
-		cout << make_integer(user_input) << " is " << make_string(user_input) << "\n";
-	} while (yes_no("\n Play again? "));
-}
 
